9ab53ca8fd19599bc9ddb7ee5e21e98420f6bac1
Xosrov <seyed.alireza.miryazdi@gmail.com>
2021-12-05 12:33:32 +0330

    added trickle ice with onicecandidate

--- main.go
+++ main.go
@@ -12,57 +12,148 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/rtcerr"
 )
 
-var stopped chan bool = make(chan bool, 1)
-var localSDP *webrtc.SessionDescription = nil
-var remoteSDP chan webrtc.SessionDescription = make(chan webrtc.SessionDescription, 1)
+type MessageType uint
 
-func serve(port string) {
+const (
+	FAILED int = 0
+	OK     int = 1
+)
+const (
+	StringType       MessageType = iota // response is just a string
+	SDPJsonType                         // response is a webrtc.SessionDescription object
+	ICECandidateType                    // response is a webrtc.IceCandidate object
+)
+
+type WsMessage struct {
+	Status   int             `json:"status"`
+	Type     MessageType     `json:"type"`
+	Response json.RawMessage `json:"response"`
+}
+
+var remoteSDP chan webrtc.SessionDescription = make(chan webrtc.SessionDescription, 1)
+var IceCandidates chan webrtc.ICECandidateInit = make(chan webrtc.ICECandidateInit, 100) // store ice candidates that arrive before remote desc is set
+var peerConnection *webrtc.PeerConnection
+var upgrader = websocket.Upgrader{}
+var incoming chan WsMessage = make(chan WsMessage, 100)
+var outgoing chan WsMessage = make(chan WsMessage, 100)
+
+func read_incoming_messages(sock *websocket.Conn) {
+	for {
+		_, message, err := sock.ReadMessage()
+		if err != nil {
+			if websocket.IsCloseError(err, websocket.CloseGoingAway) {
+				return
+			}
+			panic(err)
+		}
+		parsedMsg := WsMessage{}
+		if err := json.Unmarshal(message, &parsedMsg); err != nil {
+			fmt.Println("Unknown message type received")
+			continue
+		}
+		incoming <- parsedMsg
+	}
+}
+func parse_incoming_messages() {
+	for {
+		parsedMsg := <-incoming
+		switch parsedMsg.Type {
+		case StringType:
+			var message string
+			if err := json.Unmarshal(parsedMsg.Response, &message); err != nil {
+				fmt.Println("Bad format for string message")
+				continue
+			}
+			fmt.Printf("Got string: \"%s\"\n", message)
+		case SDPJsonType:
+			sdp := webrtc.SessionDescription{}
+			if err := json.Unmarshal(parsedMsg.Response, &sdp); err != nil {
+				fmt.Println("Bad format for sdp message")
+				continue
+			}
+			remoteSDP <- sdp
+		case ICECandidateType:
+			iceCan := webrtc.ICECandidateInit{}
+			if err := json.Unmarshal(parsedMsg.Response, &iceCan); err != nil {
+				fmt.Println("Bad format for ICE candidate")
+				continue
+			}
+			if err := peerConnection.AddICECandidate(iceCan); err != nil {
+				if errors.Is(err, rtcerr.InvalidStateError{Err: webrtc.ErrNoRemoteDescription}.Err) {
+					IceCandidates <- iceCan
+					continue
+				}
+				panic(err)
+			}
+			fmt.Println("ice candidaate set")
+		}
+	}
+}
+func write_outgoing_messages(sock *websocket.Conn) {
+	// currently only writes sdp of server
+	for {
+		parsedMsg := <-outgoing
+		json, err := json.Marshal(&parsedMsg)
+		if err != nil {
+			panic(err)
+		}
+		sock.WriteMessage(websocket.TextMessage, json)
+	}
+}
+func start_websocket_rtc(c **gin.Context) {
+	sock, err := upgrader.Upgrade((*c).Writer, (*c).Request, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		sock.Close()
+	}()
+	// do this better, create a channel to manage all at once
+	go parse_incoming_messages()
+	go write_outgoing_messages(sock)
+	go start_backend()
+	read_incoming_messages(sock) // read until closed
+
+}
+func serve(port string, restart chan bool) {
 	router := gin.Default()
 	router.Static("/static", "./view")
 	router.LoadHTMLGlob("view/html/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	router.POST("/sdp", func(c *gin.Context) {
-		decoder := json.NewDecoder(c.Request.Body)
-		answer := webrtc.SessionDescription{}
-		decoder.Decode(&answer)
-		select {
-		case remoteSDP <- answer:
-		default:
-			fmt.Println("Channel is still not emptied, disregarding new request")
+	router.GET("/ws", func(c *gin.Context) {
+		fmt.Println(c.Request.URL.Query().Get("token"))
+		fmt.Println("started socket and rtc ==============")
+		start_websocket_rtc(&c)
+		for len(incoming) > 0 {
+			<-incoming
 		}
-		fmt.Println("Got client sdp")
-		c.String(http.StatusOK, "OK")
-	})
-	router.GET("/sdp", func(c *gin.Context) {
-		// wait for local sdp to be ready
-		for localSDP == nil {
+		for len(outgoing) > 0 {
+			<-outgoing
 		}
-		c.JSON(http.StatusOK, *localSDP)
-		fmt.Println("Telling client sdp")
-		// empty for next client, not the best way but works
-		localSDP = nil
+		for len(remoteSDP) > 0 {
+			<-remoteSDP
+		}
+		for len(IceCandidates) > 0 {
+			<-IceCandidates
+		}
+		fmt.Println("stopped socket and rtc ==============")
 	})
 	router.Run(port)
+
 }
-func rtcServer() {
-	// Manage starting RTC connections and restarted connections
-	// run until stopped
-	for len(stopped) == 0 {
-		fmt.Println("running --- ---- ---- ")
-		start_backend()
-	}
-}
-func readPacket(Track *webrtc.TrackLocalStaticRTP, Listener *net.UDPConn, running *bool, res chan bool) {
+func readPacket(Track *webrtc.TrackLocalStaticRTP, Listener *net.UDPConn) {
 	inboundRTPPacket := make([]byte, 1600)
-	for *running {
+	for {
 		n, _, err := Listener.ReadFrom(inboundRTPPacket)
 		if err != nil {
-			panic(fmt.Sprintf("error during read: %s", err))
+			return
 		}
 		if _, err = Track.Write(inboundRTPPacket[:n]); err != nil {
 			if errors.Is(err, io.ErrClosedPipe) {
@@ -72,20 +163,38 @@ func readPacket(Track *webrtc.TrackLocalStaticRTP, Listener *net.UDPConn, runnin
 			panic(err)
 		}
 	}
-	res <- true
+}
+func set_ice_candidates() {
+	// set candidates in IceCandidates chan after remote desc is set
+	for candidate := range IceCandidates {
+		if err := peerConnection.AddICECandidate(candidate); err != nil {
+			return
+		}
+	}
 }
 func start_backend() {
 	// send stop signal if exit
-	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
+	pc, err := webrtc.NewPeerConnection(webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
 			{
 				URLs: []string{"stun:stun.l.google.com:19302"},
 			},
 		},
 	})
+	peerConnection = pc // set to global peerconnection object
 	if err != nil {
 		panic(err)
 	}
+	peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+		iceJson, err := json.Marshal(candidate.ToJSON())
+		if err != nil {
+			panic(err)
+		}
+		outgoing <- WsMessage{OK, ICECandidateType, iceJson}
+	})
 	// UDP video listener for RTP on port 5004
 	Vlistener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 5004})
 	if err != nil {
@@ -96,12 +205,15 @@ func start_backend() {
 	if err != nil {
 		panic(err)
 	}
-	// make sure listeners are closed after done
+	// make sure listeners and peer connection are closed after done
 	defer func() {
 		if err = Vlistener.Close(); err != nil {
 			panic(err)
 		}
 		if err = Alistener.Close(); err != nil {
+			panic(err)
+		}
+		if err = peerConnection.Close(); err != nil {
 			panic(err)
 		}
 	}()
@@ -145,55 +257,36 @@ func start_backend() {
 			}
 		}
 	}()
-
-	running := true
-	// ICE handler
-	peerConnection.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
-		fmt.Println("Connection state changed: " + state.String())
-		switch state {
-		case webrtc.ICEConnectionStateFailed:
-			if err := peerConnection.Close(); err != nil {
-				panic(err)
-			}
-			running = false
-		case webrtc.ICEConnectionStateDisconnected:
-			running = false
-		}
-
-	})
 	// get offer
 	offer := <-remoteSDP
 	if err = peerConnection.SetRemoteDescription(offer); err != nil {
 		panic(err)
 	}
+	go set_ice_candidates() // set ice candidates received before remote desc set
 	// create answer
 	answer, err := peerConnection.CreateAnswer(nil)
 	if err != nil {
 		panic(err)
 	}
-	// Create channel that is blocked until ICE Gathering is complete
-	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
-
 	// Sets the LocalDescription, and starts our UDP listeners
 	if err = peerConnection.SetLocalDescription(answer); err != nil {
 		panic(err)
 	}
-	// Block until ICE Gathering is complete, disabling trickle ICE
-	<-gatherComplete
 	// set local sdp
-	localSDP = peerConnection.LocalDescription()
+	sdpJson, err := json.Marshal(peerConnection.LocalDescription())
+	if err != nil {
+		panic(err)
+	}
+	outgoing <- WsMessage{OK, SDPJsonType, sdpJson}
 	// send RTP packets forever
-	vc := make(chan bool, 1)
-	ac := make(chan bool, 1)
-	go readPacket(videoTrack, Vlistener, &running, vc)
-	go readPacket(audioTrack, Alistener, &running, ac)
-	<-vc
-	<-ac
+	go readPacket(videoTrack, Vlistener)
+	readPacket(audioTrack, Alistener)
 }
 
 func main() {
-	go serve(":8080")
-	go rtcServer()
+	restarted := make(chan bool, 1)
+	go serve(":8080", restarted)
 	fmt.Println("started")
-	<-stopped
+	wait := make(chan bool, 1)
+	<-wait
 }
--- view/js/main.js
+++ view/js/main.js
@@ -1,13 +1,20 @@
 'use strict';
-window.onload = function() {
-    console.log("loaded page");
-    let localConnection = new RTCPeerConnection({
-        iceServers: [
-          {
-            urls: 'stun:stun.l.google.com:19302'
-          }
-        ]
-    });
+const localConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302'
+      }
+    ]
+});
+function createRTCConnection(ws) {
+    // on ice candidate
+    localConnection.onicecandidate = function(event) {
+        if (event.candidate == null) {
+            return;
+        }
+        ws.send(JSON.stringify({status: 1, type: 2, response: event.candidate}));
+    }
+    // create media stream
     const stream = new MediaStream();
     const remoteVideo = document.getElementById('stream');
     remoteVideo.srcObject = stream;
@@ -16,8 +23,8 @@ window.onload = function() {
         stream.addTrack(event.track);
     }
     localConnection.oniceconnectionstatechange = e => console.log(localConnection.iceConnectionState);
-    localConnection.addTransceiver('audio', {'direction': 'recvonly'})
-    localConnection.addTransceiver('video', {'direction': 'recvonly'})
+    localConnection.addTransceiver('audio', {'direction': 'recvonly'});
+    localConnection.addTransceiver('video', {'direction': 'recvonly'});
 
     localConnection.createOffer().then(offer => {
         localConnection.setLocalDescription(offer).then(()=>{
@@ -28,25 +35,65 @@ window.onload = function() {
             console.log("Error: "+e);
         });
     })
-    // close connection on unload
-    window.onbeforeunload = async function () {
-        localConnection.close();
-        localConnection = null;
-    }
     document.getElementById("start").addEventListener("click", function() {
         console.log("Clicked button")
         // send offer
         let offer = localConnection.localDescription;
-        axios.post("/sdp", {Type: offer.type, SDP: offer.sdp}).then(() => {
-            console.log("Sent offer");
-            // get answer
-            axios.get("/sdp").then(response=>{
-                let remoteDesc = new RTCSessionDescription(response.data);
-                localConnection.setRemoteDescription(remoteDesc).then(() => {
-                    console.log("Set Answer");
-                });
-            })
-        })
+        ws.send(JSON.stringify({status: 1, type: 1, response: offer}));
+        console.log("Sent offer");
     });
+}
+window.onload = function() {
+    console.log("loaded page");
+    
+    // create websocket connection 
+    let ws = new WebSocket("ws://localhost:8080/ws?token=ey.wirkstaufmichsowieichaufdich");
+    ws.onopen = function(evt) {
+        console.log("OPEN");
+        createRTCConnection(ws);
+    }
+    ws.onclose = function(evt) {
+        console.log("CLOSE");
+        ws = null;
+    }
+    ws.onmessage = function(evt) {
+        let responseJson = JSON.parse(evt.data);
+        console.log(responseJson);
+        if (responseJson.status == 1) {
+            if (responseJson.type == 0) { // string message
+                console.log("New message from ws: " + responseJson.response);
+            }
+            else if (responseJson.type == 1) { // sdp message
+                console.log("Got answer")
+                let remoteDesc = new RTCSessionDescription(responseJson.response);
+                localConnection.setRemoteDescription(remoteDesc).then(() => {
+                    console.log("Set answer");
+                });
+            }
+            else if (responseJson.type == 2) { // ice candidate message
+                localConnection.addIceCandidate(new RTCIceCandidate(responseJson.response)).then(()=>{
+                    console.log("Added new Ice candidate:");
+                    console.log(responseJson.response);
+                });
+                // TODO: add this
+                // TODO: research what are ice candidates doing exactly
+                // TODO: fix bugs on page refresh
+                // TODO: document why on ice candidate is required
+            }
+        }
+        else {
+            console.log("error in ws response: " + responseJson.response)
+        }
+    }
+    ws.onerror = function(evt) {
+        console.log("ERROR: " + evt.data);
+    }
+    
+    // close connection on unload
+    window.onbeforeunload = async function () {
+        localConnection.close();
+        localConnection = null;
+        // TODO: send signal to close
+    }
 
 }
